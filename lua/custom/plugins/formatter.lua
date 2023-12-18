0b10f061fe77280ac0afc896afa90bf437a10347
Adam Jacob <adam@systeminit.com>
2023-12-18 15:40:34 -0800

    eslint fix

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,5 +1,6 @@
 return {
 	'mhartington/formatter.nvim',
+	lazy = false,
 	keys = {
 		{ "<leader>f", "<cmd>Format<cr>", desc = "Format" },
 	},
@@ -11,13 +12,12 @@ return {
 				lua = {
 					require("formatter.filetypes.lua").stylua,
 				},
-				vue = function()
-					local M = {}
-					local defaults = require "formatter.defaults"
-					local util = require "formatter.util"
-					M.vue = util.withl(defaults.eslint_d, "vue")
-					return M
-				end,
+				vue = {
+					require("formatter.filetypes.typescript").eslint_d,
+				},
+				typescript = {
+					require("formatter.filetypes.typescript").eslint_d,
+				}
 			},
 			["*"] = {
 				require("formatter.filetypes.any").remove_trailing_whitespace,
